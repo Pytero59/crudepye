b3fa4cef22a6b576a862f37df06746debb7ce444
projeto inicial <pietro.turelly@gmail.com>
2024-09-05 21:00:41 -0300

    Aula 8

---
+++ lib/data/dummy_users.dart
@@ -0,0 +1,22 @@
+import '../../../../../flutter/packages/flutter/lib/src/widgets/platform_view.dart';
+import '../models/user.dart1';
+
+Map DUMMY_USERS = {
+  
+  '1': const User(
+    id: '1',
+    name: 'Maria',
+    email: 'maria@alunos.com.br'
+    avatarUrl: ''
+
+
+    
+    
+  ),
+  
+  
+  
+  
+  
+  
+}
