6ee6e1a248dfa222cfd6920b8f52f76b4a0c1a72
projeto inicial <pietro.turelly@gmail.com>
2024-09-05 21:36:55 -0300

    Aula 8 Complete.

--- lib/data/dummy_users.dart
+++ lib/data/dummy_users.dart
@@ -1,22 +1,33 @@
-import '../../../../../flutter/packages/flutter/lib/src/widgets/platform_view.dart';
-import '../models/user.dart1';
+import '../models/user.dart';
 
 Map DUMMY_USERS = {
   
   '1': const User(
     id: '1',
     name: 'Maria',
-    email: 'maria@alunos.com.br'
-    avatarUrl: ''
-
-
-    
-    
+    email: 'maria@alunos.com.br',
+    avatarUrl: 'https://tpucdn.com/gpu-specs/images/c/3774-front.jpg'
   ),
-  
-  
-  
-  
-  
-  
-}
+
+  '2': const User(
+      id: '2',
+      name: 'Guilherme',
+      email: 'guilherme@alunos.com.br',
+      avatarUrl: 'https://tpucdn.com/gpu-specs/images/g/926-navi-23-xt.jpg'
+  ),
+
+
+  '3': const User(
+      id: '3',
+      name: 'Mateus',
+      email: 'mateus@alunos.com.br',
+      avatarUrl: 'https://tpucdn.com/gpu-specs/images/c/3774-top.jpg'
+  ),
+
+  '4': const User(
+      id: '4',
+      name: 'Lívia',
+      email: 'Lívia@alunos.com.br',
+      avatarUrl: 'https://tpucdn.com/gpu-specs/images/b/9187-front.small.jpg'
+  ),
+};
